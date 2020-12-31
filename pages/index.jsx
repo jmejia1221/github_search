a975ad5e7f57ccf5809d7249fc6b82f36aaad135
John Mejia <jhondario.09@gmail.com>
2020-12-31 03:07:26 -0500

    * Added new tabs components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+
+// Components
+import Tabs from '../components/Tabs';
 
 // CSS
 import styles from './index.module.scss';
@@ -6,7 +11,16 @@ import styles from './index.module.scss';
 const Home = () => {
     return (
         <section className={styles.main}>
-            hey
+            <aside>
+                <Tabs tabDefault="users"> 
+                    <div label="users" icon={faUserPlus} onlyIcon> 
+                        users
+                    </div> 
+                    <div label="repositories" icon={faGithubAlt} onlyIcon> 
+                        search Repo
+                    </div> 
+                </Tabs> 
+            </aside>
         </section>
     );
 };
